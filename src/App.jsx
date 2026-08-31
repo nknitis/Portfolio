18d24221718f8a1b76165bd289e06e110a1c57d6
Nitish Nitish <nkcode.789@gmail.com>
2026-08-31 09:31:52 +0530

    Portfolio V2 interactive redesign

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import Header from './components/Header';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import './style.css';
-
-const App = () => {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-      <Route path="/" element={<Home />} />
-        <Route path="/" element={<About />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      
-      <Footer />
-    </Router>
-  );
-};
-
-export default App;
+import React from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import "./style.css";
+export default function App(){return <div className="app"><Header/><main><Home/><About/><Projects/><Contact/></main><Footer/></div>}
